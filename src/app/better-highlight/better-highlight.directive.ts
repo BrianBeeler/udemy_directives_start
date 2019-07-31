90c476309b05afc04a8bbc09987aa1ce577eeb6f
Brian Beeler <brianlbeeler@gmail.com>
2019-07-31 16:22:20 -0500

    Learned ng-switch.

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,16 +1,38 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, 
+         OnInit, 
+         ElementRef, 
+         Renderer2, 
+         HostListener,
+         HostBinding,  
+         Input
+        } from '@angular/core';
+import { NgModuleResolver } from '@angular/compiler';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+  // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
 
   constructor(private elRef: ElementRef, private renderer:Renderer2) { 
 
   }
 
   ngOnInit() {
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
 
 }
